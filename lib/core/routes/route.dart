e29f372542cc3d6cb2b2f45881740ea5f4f46299
Geril Valdo <gerilmanday@gmail.com>
2025-02-08 23:22:18 +0700

    Update Routes

--- lib/core/routes/route.dart
+++ lib/core/routes/route.dart
@@ -3,10 +3,14 @@ import 'package:task_flutter_rwid/features/main/main_screen.dart';
 import '../../features/profile/profile_page.dart';
 import '../../features/saved/saved_page.dart';
 import '../../features/home/home_page.dart';
+import '../../features/auth/login_page.dart';
+import '../../features/auth/register_page.dart';
 
 Map<String, Widget Function(BuildContext)> routesApp = {
   '/': (context) => MainScreen(),
   '/home-page': (context) => HomePage(),
   '/saved-page': (context) => SavedPage(),
   '/profile-page': (context) => ProfilePage(),
+  '/sign-in': (context) => LoginPage(),
+  '/sign-up': (context) => RegisterPage(),
 };
